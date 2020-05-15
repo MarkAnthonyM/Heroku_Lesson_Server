7de516715d1a657dffd23b1f2b6b15f21fc6709e
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-05-15 14:16:50 -0500

    feat: add concurrency support

--- heroku_server/BookmarkServer.py
+++ heroku_server/BookmarkServer.py
@@ -44,6 +44,8 @@
 import http.server
 import requests
 import os
+from socketserver import ThreadingMixIn
+import threading
 from urllib.parse import unquote, parse_qs
 
 memory = {}
@@ -97,6 +99,10 @@ def CheckURI(uri, timeout=5):
     except requests.RequestException:
         # If the GET request raised an exception, it's not OK.
         return False
+
+
+class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
+    "This is an HTTPServer that supports thread-based concurrency."
 
 
 class Shortener(http.server.BaseHTTPRequestHandler):
@@ -173,5 +179,5 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))  # Use port if it is there
     server_address = ('', port)
-    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd = ThreadHTTPServer(server_address, Shortener)
     httpd.serve_forever()
